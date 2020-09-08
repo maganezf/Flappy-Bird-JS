51ac72e1f893a1bb3de5bc79e81c9ab980d679c7
maganezf <maganezf@kaamu.laptopPC>
2020-09-08 11:26:09 -0300

    Update 3 - Floor and Wing Infinites

--- game.js
+++ game.js
@@ -1,38 +1,60 @@
 console.log('Flappy Bird - @maganezf');
 
+let frames = 0;
+
+const hit_sound = new Audio();
+hit_sound.src = './sounds/hit.wav'
+
 const sources = new Image();
 sources.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
+function createFloor() {
 
-const floor = {
-  sourceX: 0,
-  sourceY: 610,
-  width: 224,
-  height: 112,
-  x: 0,
-  y: canvas.height - 112,
+  const floor = {
+    sourceX: 0,
+    sourceY: 610,
+    width: 224,
+    height: 112,
+    x: 0,
+    y: canvas.height - 112,
+  
+    refresh() {
+      const movedFloor = 1;
+      const repeatFloor = floor.width / 2;
+      const movementFloor = floor.x - movedFloor;
 
-  create() {
-    context.drawImage(
-      sources,
-      floor.sourceX, floor.sourceY,
-      floor.width, floor.height,
-      floor.x, floor.y,
-      floor.width, floor.height
-    );
+      /* console.log('[floor.x]', floor.x);
+      console.log('[repeatFloor]', repeatFloor);
+      console.log('[movementFloor]', movementFloor); */
 
-    context.drawImage(
-      sources,
-      floor.sourceX, floor.sourceY,
-      floor.width, floor.height,
-      floor.x + floor.width, floor.y,
-      floor.width, floor.height
-    );
+      floor.x = movementFloor % repeatFloor;
+    },
+  
+    create() {
+      context.drawImage(
+        sources,
+        floor.sourceX, floor.sourceY,
+        floor.width, floor.height,
+        floor.x, floor.y,
+        floor.width, floor.height
+      );
+  
+      context.drawImage(
+        sources,
+        floor.sourceX, floor.sourceY,
+        floor.width, floor.height,
+        floor.x + floor.width, floor.y,
+        floor.width, floor.height
+      );
+    }
   }
+  return floor;
+
 }
+
 
 const background = {
   sourceX: 390,
@@ -64,31 +86,88 @@ const background = {
   }
 }
 
-const flappyBird = {
-  sourceX: 0,
-  sourceY: 0,
-  width: 33,
-  height: 24,
-  x: 10,
-  y: 50,
-  gravity: 0.25,
-  speed: 0,
+function madeCollision(flappyBird, floor) {
+  const flappyBirdY = flappyBird.y + flappyBird.height;
+  const floorY = floor.y;
 
-  refresh() {
-    flappyBird.speed = flappyBird.speed + flappyBird.gravity;
-    flappyBird.y = flappyBird.y + flappyBird.speed;
-  },
-
-  create() {
-    context.drawImage(
-      sources,
-      flappyBird.sourceX, flappyBird.sourceY,
-      flappyBird.width, flappyBird.height,
-      flappyBird.x, flappyBird.y,
-      flappyBird.width, flappyBird.height
-    );
+  if (flappyBirdY >= floorY) {
+    return true;
+  } else {
+    return false;
   }
 }
+
+function createFlappyBird() {
+
+  const flappyBird = {
+    sourceX: 0,
+    sourceY: 0,
+    width: 33,
+    height: 24,
+    x: 10,
+    y: 50,
+    gravity: 0.25,
+    speed: 0,
+    jump_set: 4.6,
+    
+    jump() {
+      console.log("[antes]", flappyBird.speed);
+      flappyBird.speed = -flappyBird.jump_set;
+      console.log("[depois]", flappyBird.speed);
+    },
+  
+    refresh() {
+  
+      if (madeCollision(flappyBird, globals.floor)){
+        //made a collision
+        hit_sound.play();
+        switchScreenOfGame(start);
+        return;
+      }
+      
+      flappyBird.speed = flappyBird.speed + flappyBird.gravity;
+      flappyBird.y = flappyBird.y + flappyBird.speed;
+    },
+
+    movementsWing: [
+      { sourceX: 0, sourceY: 0 },    //Wing Up
+      { sourceX: 0, sourceY: 25 },   //Wing mid
+      { sourceX: 0, sourceY: 51 },   //Wing down
+      { sourceX: 0, sourceY: 26 },   //wing mid 
+    ],
+
+    currentFrames: 0,
+    
+    refreshCurrentFrames () {
+      const framesInterval = 10;
+      const intervalExceeded = frames % framesInterval === 0;
+      console.log('[passou intervalo]', intervalExceeded);
+      
+      if (intervalExceeded) {
+        const initIncrement = 1;
+        const increment = initIncrement + flappyBird.currentFrames;
+        const repeatIncrement = flappyBird.movementsWing.length;
+        flappyBird.currentFrames = increment % repeatIncrement;
+      }
+    },
+    
+    create() {
+      flappyBird.refreshCurrentFrames();
+
+      const { sourceX, sourceY } = flappyBird.movementsWing[flappyBird.currentFrames];
+
+      context.drawImage(
+        sources,
+        sourceX, sourceY,
+        flappyBird.width, flappyBird.height,
+        flappyBird.x, flappyBird.y,
+        flappyBird.width, flappyBird.height
+      );
+    }
+  }
+  return flappyBird;
+}
+
 
 const homeScreen = {
   sourceX: 134,
@@ -109,18 +188,31 @@ const homeScreen = {
   }
 }
 
+const globals = {};
 let currentScreen = {};
 
-function switchScreenOfGame (screen) {
-  currentScreen = screen;
+function switchScreenOfGame (newScreen) {
+  currentScreen = newScreen;
+  
+  if (currentScreen.initialize){
+    currentScreen.initialize();
+  }
 }
 
 const screens = {
+
   start: {
+
+    initialize(){
+      globals.flappyBird = createFlappyBird();
+      globals.floor = createFloor();
+    },
+
     create () {
       background.create();
-      floor.create();
-      flappyBird.create();
+      globals.flappyBird.create();
+      globals.floor.create();
+
       homeScreen.create();
     },
 
@@ -129,7 +221,7 @@ const screens = {
     },
 
     refresh() {
-      
+      globals.floor.refresh();
     }
   }
    
@@ -138,19 +230,25 @@ const screens = {
 screens.Game = {
   create () {
     background.create();
-    floor.create();
-    flappyBird.create();
+    globals.floor.create();
+    globals.flappyBird.create();
+  },
+
+  click(){
+    globals.flappyBird.jump();
   },
 
   refresh() {
-    flappyBird.refresh();
-  }
+    globals.flappyBird.refresh();
+   }
 }
 
 function loop () {
   
   currentScreen.create();
   currentScreen.refresh();
+
+  frames = frames + 1;
 
   requestAnimationFrame(loop);
 }
